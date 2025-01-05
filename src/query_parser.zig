e5fec6fad47fe45c52a4f64cf375204929e04be3
Dias Kalkamanov <diicellman@gmail.com>
2025-01-05 15:47:13 +0500

    [any message]

--- src/query_parser.zig
+++ src/query_parser.zig
@@ -11,17 +11,20 @@ pub fn parseQuery(allocator: std.mem.Allocator, sql: []const u8) !Query {
     var it = std.mem.splitSequence(u8, sql, " ");
 
     // skip "SELECT"
-    _ = it.next() orelse return error.InvalidQuery;
+    const select_word = it.next() orelse return error.InvalidQuery;
+    if (!std.ascii.eqlIgnoreCase(select_word, "SELECT")) {
+        return error.InvalidQuery;
+    }
 
     // check for COUNT(*)
     const count = it.next() orelse return error.InvalidQuery;
-    if (!std.mem.eql(u8, count, "COUNT(*)")) {
+    if (!std.ascii.eqlIgnoreCase(count, "COUNT(*)")) {
         return error.InvalidQuery;
     }
 
     // skip "FROM"
     const from = it.next() orelse return error.InvalidQuery;
-    if (!std.mem.eql(u8, from, "FROM")) {
+    if (!std.ascii.eqlIgnoreCase(from, "FROM")) {
         return error.InvalidQuery;
     }
 
